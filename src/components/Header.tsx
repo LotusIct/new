ebca594e3940f0d137f8c571e8faae069f46dd05
LotusIct <michaela.galbiati@lotusict.com>
2025-10-21 15:59:40 -0300

    Primeiro commit do projeto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Header.css";
 
 export default function Header() {
--- src/components/Locais.tsx
+++ src/components/Locais.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Locais.css";
 
 export default function Locais() {
